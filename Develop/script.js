cc84ae1b51014d249e1db0261dffe77902f156fd
Huiran Lin <hugh80082@gmail.com>
2021-06-24 15:42:55 -0700

    add database for the password

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,11 @@ var lengthSoFar = 0;
 var totalLength = 0;
 const allCriteria = [ "lowercase", "uppercase", "numeric", "special characters"];
 
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+var special_characters = ' !"#$%& ' + ' ()*+,-./:;<=>?@[\]^_`{|}~ ';
+
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
