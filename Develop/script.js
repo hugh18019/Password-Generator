ac352fc8c4242c961b8648afe10b9c4757c8ff4b
Huiran Lin <hugh80082@gmail.com>
2021-06-24 15:41:44 -0700

    add global variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,8 @@
+var lengthSoFar = 0;
+var totalLength = 0;
+const allCriteria = [ "lowercase", "uppercase", "numeric", "special characters"];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +12,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
